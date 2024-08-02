8e634c3c4d1b1ba70d82f92387acc2a76fbc026a
Prakhar Pandey <124185531+Prakharpandey007@users.noreply.github.com>
2024-08-02 22:52:50 +0530

    Time: 12 ms (56.64%) | Memory: 43.2 MB (56.14%) - LeetSync

---
+++ 2977-check-if-a-string-is-an-acronym-of-words/check-if-a-string-is-an-acronym-of-words.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isAcronym(vector<string>& words, string s) {
+       string st;
+       for(int i=0;i<words.size();i++){
+        st.push_back(words[i][0]);
+       }
+       if(st==s){
+        return true;
+       }
+       else {
+        return false;
+       }
+    }
+};
