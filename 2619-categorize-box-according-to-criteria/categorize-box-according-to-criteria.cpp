47134cd808b81f23372872d10e50f1b456b32d8e
Prakhar Pandey <124185531+Prakharpandey007@users.noreply.github.com>
2024-08-14 12:10:11 +0530

    Time: 2 ms (57.64%) | Memory: 7.4 MB (10.95%) - LeetSync

---
+++ 2619-categorize-box-according-to-criteria/categorize-box-according-to-criteria.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string categorizeBox(int length, int width, int height, int mass) {
+       bool bulky =((long long)length*width*height >= 1e9)||length>=1e4||width>=1e4||height>=1e4;
+       bool heavy=mass >= 100;
+       if(bulky && heavy) {
+        return "Both";
+       }
+       else if(!bulky && !heavy) 
+       {
+        return "Neither";
+       }
+       else if(bulky) {
+        return "Bulky";
+       }
+       else {
+        return "Heavy";
+       }
+    }
+};
